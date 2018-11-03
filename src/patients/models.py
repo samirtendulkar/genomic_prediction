9ac3627a93ddcab201d29cc2fd4e0539aff9a6b7
Samir Tendulkar <samnik_12345@hotmail.com>
2018-11-03 04:18:21 -0400

    Added models to patients app

--- src/patients/models.py
+++ src/patients/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Patients(models.Model):
+    patient = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=35)
+    phone = models.CharField(max_length=18)
+    email = models.EmailField
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Embryo(models.Model):
+    code_name = models.CharField(max_length=100)
+    karyotype = models.CharField(max_length=100)
+    down_syndrome = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    GENDER_CHOICES = (
+        ("M", "Male"),
+        ("F", "Female"),
+
+    )
+    sex = models.CharField(max_length=1, choices=GENDER_CHOICES)
+
+
+
+
+
